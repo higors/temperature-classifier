fc7dae7b14f38501e25ccf2b3d40bb56cd19c64d
Higor Santos <higormatheus_28@hotmail.com>
2018-11-08 18:42:31 -0200

    Add folder source

--- src/knn-algorithm-classifier.py
+++ src/knn-algorithm-classifier.py
@@ -1,41 +1,39 @@
 import pickle
-import numpy as np
 
 import matplotlib.pyplot as plt
-import process_dataset
-from sklearn import svm
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
-import confusion_matrix
+from src import confusion_matrix, process_dataset
 
 path = './csv/classified-temperatures.csv'
-
 data_set_processed = process_dataset.execute(path)
 # process_data_set.plot_statistics(data_set_processed['classes_name'], data_set_processed['cold_class'],
 #                                 data_set_processed['neutral_class'], data_set_processed['warm_class'])
 
 
-def train_svm_algorithm(temps, labels, classes_name):
-    train_temperatures, test_temperatures, train_labels, test_labels = train_test_split(temps, labels,
+def train_knn_algorithm(temps, all_labels, classes):
+    train_temperatures, test_temperatures, train_labels, test_labels = train_test_split(temps, all_labels,
                                                                                         test_size=0.25,
                                                                                         random_state=25)
     train_temperatures = train_temperatures.reshape(-1, 1)
     test_temperatures = test_temperatures.reshape(-1, 1)
-    # create svm algorithm
-    svm_classifier = svm.SVC(kernel='linear', C=1.0)
+    # create knn algorithm
+    knn_algorithm_classifier = KNeighborsClassifier(n_neighbors=3)
 
-    # fit and serialize the algorithm svm
-    trained_svm = svm_classifier.fit(np.array(train_temperatures), train_labels)
-    pickle.dump(trained_svm, open("trained-svm-algorithm.p", "wb"))
+    # fit and serialize the algorithm knn
+    trained_svm = knn_algorithm_classifier.fit(np.array(train_temperatures), train_labels)
+    pickle.dump(trained_svm, open("trained-knn-algorithm.p", "wb"))
 
     # validate the algorithm training
     classified_temperatures = trained_svm.predict(test_temperatures)
 
     np.set_printoptions(precision=2)
     plt.figure()
-    confusion_matrix.execute(test_labels, classified_temperatures, classes=classes_name, title='Confusion Matrix')
+    confusion_matrix.execute(test_labels, classified_temperatures, classes=classes, title='Confusion Matrix')
     plt.show()
-    print("Accuracy: %s" % svm_classifier.score(test_temperatures, test_labels))
+    print("Accuracy: %s" % knn_algorithm_classifier.score(test_temperatures, test_labels))
 
 
-train_svm_algorithm(data_set_processed['temperatures'], data_set_processed['labels'], data_set_processed['classes_name'])
+train_knn_algorithm(data_set_processed['temperatures'], data_set_processed['labels'], data_set_processed['classes_name'])
