163064b6df4e6e4965dfb3993a6586ded2cf9643
higors <higormatheus_28@hotmail.com>
2018-11-09 20:49:29 -0200

    Add graphics

--- src/resources/statistics.py
+++ src/resources/statistics.py
@@ -5,8 +5,12 @@ import numpy as np
 def plot_statistics(classes_names, class_one, class_two, class_three):
     y_pos = np.arange(len(classes_names))
     performance = [class_one, class_two, class_three]
+    x = "Cold" + "[" + str(class_one) + "]"
+    y = "Neutral" + "[" + str(class_two) + "]"
+    z = "Warn" + "[" + str(class_three) + "]"
+    names = [str(x), str(y), str(z)]
     plt.bar(classes_names, performance, align='center', alpha=0.9)
-    plt.xticks(y_pos, classes_names)
+    plt.xticks(y_pos, names)
     plt.ylabel('Amount')
     plt.title('Amount per class ')
     plt.show()
